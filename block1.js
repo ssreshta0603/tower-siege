a5c847dbf6f4dbee60fc185ee8e6d6df62c93dca
ssreshta0603 <67061788+ssreshta0603@users.noreply.github.com>
2020-08-02 23:02:10 +0530

    Update block1.js

--- block1.js
+++ block1.js
@@ -10,18 +10,23 @@ class Block1 {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-    
+      this.visibility = 255;
     World.add(world, this.body);
   }
   display(){
-    var angle = this.body.angle;
-    push();
-    fill("lightblue");
-    translate(this.body.position.x, this.body.position.y);
-    rotate(angle);
-    rectMode(CENTER);
-    rect( 0, 0, this.width, this.height);
-    pop();
+    if (this.body.speed < 6) {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("grey");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+    else {
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    };
   }
 }
     
