316a4107d2c551f1ed58de7f6134d8f82105feed
ssreshta0603 <67061788+ssreshta0603@users.noreply.github.com>
2020-08-03 08:59:02 +0530

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -1,30 +1,23 @@
 class Polygon{
-    constructor(x, y, radius, angle) {
+    constructor(x,y) {
         var options = {
             restitution:0.5,
             friction:1.0,
             density:1.0
         }
-        this.body = Bodies.circle(x,y,radius,options);
-        this.radius = radius
+        this.body = Bodies.circle(x,y,35,options);
+        
+        this.image = image
         this.image = loadImage("polygon.png");
-        this.visiblity = 255
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
-       
-if(this.body.speed<3 ){
-  super.display();
-}
-else{
-  World.remove(world,this.body);
-  push();
-  this.visiblity = this.visiblity-5
-  tint(255,this.visiblity);
-  image(this.image,this.body.position.x,this.body.position.y);
-  pop();
-}
+      
+        push();
+        var pos = this.body.position;
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image,0,0,50,50);
+        pop();
       }
 }
-
