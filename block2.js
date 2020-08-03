d63ae4a6aed0db1ac2df4f4ac5d63507a342f7e8
ssreshta0603 <67061788+ssreshta0603@users.noreply.github.com>
2020-08-03 09:00:24 +0530

    Update block2.js

--- block2.js
+++ block2.js
@@ -10,18 +10,27 @@ class Block2 {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-    
+    this.visibility = 255;
     World.add(world, this.body);
   }
   display(){
-    var angle = this.body.angle;
-    push();
-    fill("pink");
-    translate(this.body.position.x, this.body.position.y);
-    rotate(angle);
-    rectMode(CENTER);
-    rect( 0, 0, this.width, this.height);
-    pop();
+    if (this.body.speed < 3) {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+    else {
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    }
+
+    
+  
   }
 }
+    
     
